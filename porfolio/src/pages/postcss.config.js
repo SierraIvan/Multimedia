257a933e55f20cfb7277e02a6780329b5416349c
Iván Sierra <ivan.sierra.perez@colegiolitterator.com>
2024-11-10 16:00:36 +0100

    incorporar astro

---
+++ porfolio/src/pages/postcss.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Incluye todas las rutas de archivos usados en Astro
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class", // Habilitar modo oscuro basado en clase
+  plugins: [],
+};
